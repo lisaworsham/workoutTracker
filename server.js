fbe51085125c18e54ae30b1bdb3ddaa839bb73a5
Lisa Worsham <lisa.r.hyndman@gmail.com>
2020-12-08 16:28:20 -0600

    updated appuse routes in serverjs

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api"));
-app.use(require("./routes/html"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 
 app.listen(PORT, () => {
